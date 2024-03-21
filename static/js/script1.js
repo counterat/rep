0bbb94fe8d60af3933bc5baa8fa7472090390b04
Yuriy Zholtov <125594698+counterat@users.noreply.github.com>
2024-03-21 21:00:47 +0200

    Add files via upload

--- static/js/script1.js
+++ static/js/script1.js
@@ -94,8 +94,8 @@ var socket = io.connect(`${window.location.protocol}//${window.location.hostname
 		     var WebApp = window.Telegram.WebApp; 
 	
 
-        var tgusername =WebApp.initDataUnsafe.user.username;
-            var ipAdress =  WebApp.initDataUnsafe.user.id;
+        var tgusername = WebApp.initDataUnsafe.user.username;/* document.querySelector('#ipAdress').value;  */   
+            var ipAdress = WebApp.initDataUnsafe.user.id; /* document.querySelector('#ipAdress').value;  */    
 
 	
 		
@@ -146,10 +146,56 @@ xhr.send(jsonData);
 		
 	
            
- 
-get_bets()
 
+get_bets()
+get_previous_xes()
         });
+
+function get_previous_xes(){
+    var xhr = new XMLHttpRequest();
+
+// Указываем метод и URL для запроса
+xhr.open("GET", "/get_previous_xes", true);
+
+// Устанавливаем заголовок Content-Type для передачи данных в формате JSON
+xhr.setRequestHeader("Content-Type", "application/json");
+
+// Определяем функцию, которая будет вызвана при завершении запроса
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    var data = JSON.parse(xhr.response);
+    var previous_xes = data.data.reverse()
+    previous_xes.forEach(element => {
+        if (parseFloat(element) <1.05){
+
+colForCoefs.innerHTML =` <div class="coeff_lose" >
+<span>${parseFloat( element)+'x'}</span>
+</div>${colForCoefs.innerHTML}`
+
+}
+else if (parseFloat(element) >= 2){
+
+colForCoefs.innerHTML = `<div class="coeff_big_win" >
+<span>${parseFloat(parseFloat( element)+'x')}</span>
+</div>${colForCoefs.innerHTML}`
+}
+else{
+
+colForCoefs.innerHTML =   `<div class="coeff_win" style="border-radius: 47px;
+background-color: #ffffff;  display: flex; justify-content: center; align-items: center; height: 5vh;
+border: 3px solid #65a7fb; color: #65a7fb;text-align: center; width: 18%; font-weight: 500; flex: 0 0 20%;">
+<span>${parseFloat( element)+'x'}</span>   
+</div>${colForCoefs.innerHTML}`
+}
+    });
+
+
+  }
+
+}
+xhr.send();
+}
+
 
 function get_bets(){
 
@@ -166,7 +212,7 @@ xhr.onreadystatechange = function () {
   if (xhr.readyState === XMLHttpRequest.DONE) {
     
     var newData = JSON.parse(xhr.response);
-    
+
     if (newData['bets']){
              
              newData['bets'].forEach(bet => {
